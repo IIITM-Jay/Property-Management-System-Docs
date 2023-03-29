734aedb24111747bb2137c440bcc4e002d8f686e
Jay Dev Jha <jaydev.neuroscitech@gmail.com>
2023-03-29 16:01:20 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,6 +27,15 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+
+html_css_files = [
+    'css/custom.css',
+]
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
